093914f952aeba01a2332c9449e9c6117fb665e9
aadhi-techie <tmis.aadhisiva.s@gmail.com>
2026-06-29 12:54:34 +0530

    feat: add PWA support with vite-plugin-pwa and update package.json

--- Meenakshi/vite.config.js
+++ Meenakshi/vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa' // இதைச் சேர்க்கவும்
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.webp', 'masked-icon.svg'],
+      manifest: {
+        name: 'Sree Meenakshi Glass & Plywoods',
+        short_name: 'SreeMeenakshi',
+        description: 'Premium Glass, Plywood, and UPVC supplier in Chennai',
+        theme_color: '#f97316', // உங்களோட பிராண்ட் கலர்
+        icons: [
+          {
+            src: 'apple-touch-icon.webp',
+            sizes: '192x192',
+            type: 'image/webp'
+          },
+          {
+            src: 'apple-touch-icon.webp',
+            sizes: '512x512',
+            type: 'image/webp'
+          }
+        ]
+      }
+    })
+  ]
 })
